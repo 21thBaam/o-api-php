b488514a966046ec0634551863d5f15ab15e5446
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-17 20:10:39 -0500

    Add files via upload

--- api.php
+++ api.php
@@ -1,11 +1,31 @@
 <?php
 require '../logica/conexion.php';
 
-$sql = "INSERT INTO ventas () VALUES() ";
-if ($conn->query($sql) === TRUE) {
-	echo "New record created successfully";
-} else {
-	echo "Error: " . $sql . "<br>" . $conn->error;
+header("HTTP/1.1 200 OK");
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+	echo "GET";
+	exit();
 }
 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	/*$sql = "INSERT INTO $_POST['dir'] (nombre, apellido) VALUES() ";
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}*/
+	echo "POST";
+	exit();
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'PUT'){
+	echo "PUT";
+	exit();
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'DELETE'){
+	echo "DELETE";
+	exit();
+}
 ?>
