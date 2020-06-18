1c238b3840f7137c2431b0709f61d38923d24b10
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-17 19:40:09 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,12 +1,13 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "camposgranos";
+    $servername = "iwqrvsv8e5fz4uni.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $username = "el0gnvg0jiqbftm3";
+    $password = "xb76s2sub1phyj7q";
+    $dbname = "dspnarlgltkkzpxo";
     
     $conn = new mysqli($servername, $username, $password, $dbname);
     
     if($conn->connect_error){
         die("Connection failed: ".$conn->connect_error);
     }
+    echo "Conectado";
 ?>
