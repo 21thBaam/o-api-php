6ffd7e4bf49f7861757f0a787e83088398c7ec1a
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-17 21:01:20 -0500

    Update api.php

--- api.php
+++ api.php
@@ -4,43 +4,15 @@ require 'conexion.php';
 header("HTTP/1.1 200 OK");
 
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
-
-    $sql = "SELECT * FROM prueba";
-    $result = mysqli_query($conn, $sql);
-    $iteraciones=0;
-
-    if (mysqli_num_rows($result) > 0) {
-        // output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
-            $resultado[$iteraciones] = array_merge($row);
-            $iteraciones++;
-        }
-    } else {
-        echo "0 results";
-        $resultado = 0;
-    }
-    mysqli_close($conn);
-
-    var_dump($resultado);
-	/*
-	$sth = mysqli_query("SELECT * FROM prueba");
+	$sth = mysqli_query($conn,"SELECT * FROM prueba");
 	$rows = array();
-	$iteraciones=0;
-	$resultado[$iteraciones] = array_merge($row);
-    $iteraciones++;
 	while($r = mysqli_fetch_assoc($sth)) {
 		echo "id: " . $r["id"]. " - Name: " . $r["nombre"]. " " . $r["apellido"]. "<br>";
 		$rows[] = array_merge($r);
-		$resultado[$iteraciones] = array_merge($row);
-		$iteraciones++;
 	}
 	print json_encode($rows);
 	var_dump($rows);
-	
-	echo "---------------";
-	
-	var_dump($resultado);*/
-	
+	echo "<br>---------------<br>";
 	exit();
 }
 
