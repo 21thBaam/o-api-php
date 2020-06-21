73e269741eb7502a46f24276ae6847852f0e0135
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-20 20:08:35 -0500

    Update api.php

--- api.php
+++ api.php
@@ -33,15 +33,16 @@ if($_SERVER['REQUEST_METHOD'] == 'PUT'){
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'DELETE'){
-	//echo "DELETE";
-	$post_vars;
-	parse_str(file_get_contents("php://input"),$post_vars);
-	$sql = "DELETE FROM prueba WHERE id = '$post_vars[id]' ";
+	echo "DELETE";
+	$data = file_get_contents("php://input");
+	var_dump($data);
+	//parse_str(file_get_contents("php://input"),$post_vars);
+	/*$sql = "DELETE FROM prueba WHERE id = '$post_vars[id]' ";
 	if($conn->query($sql) === TRUE){
 		echo "Deleted"
 	}else{
 		echo "Error: " . $sql . "<br>" . $conn->error;
-	}
+	}*/
 	exit();
 }
 ?>
