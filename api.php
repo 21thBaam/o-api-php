5817f25b0ec908d2acfa2ad33bb5f1cba8a87c4d
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-20 20:22:29 -0500

    Update api.php

--- api.php
+++ api.php
@@ -37,8 +37,8 @@ if($_SERVER['REQUEST_METHOD'] == 'DELETE'){
 	parse_str(file_get_contents("php://input"),$data);
 	var_dump($data);
 	echo "<br>".$data['id'];
-	$sql = "DELETE FROM prueba WHERE id = '$data[id]' ";
-	echo "<br>".$sql
+	//$sql = "DELETE FROM prueba WHERE id = '$data[id]' ";
+	//echo "<br>".$sql
 	/*if($conn->query($sql) === TRUE){
 		echo "Deleted"
 	}else{
