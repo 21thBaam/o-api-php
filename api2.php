f21bc2d35c1a990b0bc402b1a844a71ed0ba8605
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-22 10:15:18 -0500

    Add files via upload

---
+++ api2.php
@@ -0,0 +1,38 @@
+<?php
+require 'conexion.php';
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('content-type: application/json; charset=utf-8');
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+	$sth = mysqli_query($conn,"SELECT * FROM tipoUsuario");
+	$rows = array();
+	while($r = mysqli_fetch_assoc($sth)) {
+		$rows[] = array_merge($r);
+	}
+	print json_encode($rows);
+	exit();
+}
+
+/*if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	$sql = "INSERT INTO prueba (nombre, apellido) VALUES('$_POST[nombre]', '$_POST[apellido]') ";
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	exit();
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'PUT'){
+	echo "PUT";
+	exit();
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'DELETE'){
+	echo "DELETE";
+	exit();
+}*/
+?>
