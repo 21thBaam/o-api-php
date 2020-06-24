8ebd31e9f5b52d93c4413d4368f01f64b7d41465
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-24 00:10:57 -0500

    Update api.php

--- api.php
+++ api.php
@@ -22,10 +22,19 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$info = (parse_url($url, PHP_URL_QUERY));
 	parse_str($info, $get_array);
 	//print_r($get_array);
-	echo "Nombre: ",$get_array["nombre"],"Apellido: ",$get_array["apellido"];
-	//$sql = "INSERT INTO prueba SET nombre = '$get_array[nombre]', apellido = '$get_array[apellido]';
+	//echo "Nombre: ",$get_array["nombre"],"Apellido: ",$get_array["apellido"];
 	$sql = "INSERT INTO prueba SET nombre='$get_array[nombre]', apellido='$get_array[apellido]' ";
-	mysqli_query($conn,$sql);
+	if (mysqli_query($conn,$sql) === TRUE) {
+		echo "New record created successfully";
+		$result->status = "success";
+		$result->message = "User Added";
+		print json_encode($result);
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+		$result->status = "failed";
+		$result->message = "User Not Added";
+		print json_encode($result)
+	}
 	exit();
 }
 
