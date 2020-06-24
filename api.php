e49da9678f35a5736d012fc7d141f8a3a5ebb2c7
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-23 21:10:38 -0500

    Update api.php

--- api.php
+++ api.php
@@ -23,6 +23,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	echo "  :::  ";
 	parse_str(file_get_contents("php://input"),$data2);
 	var_dump($data2);*/
+	$url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 	var_dump(parse_url($url, PHP_URL_QUERY));
 	exit();
 }
