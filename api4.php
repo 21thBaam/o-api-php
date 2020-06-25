6f767e2bb5bb231aacc5c59c17547d097d39a9a6
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-24 23:14:15 -0500

    Update api4.php

--- api4.php
+++ api4.php
@@ -6,29 +6,13 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('content-type: application/json; charset=utf-8');
 
-if($_SERVER['REQUEST_METHOD'] == 'GET'){
-	$sth = mysqli_query($conn,"SELECT * FROM usuario");
-	$rows = array();
-	while($r = mysqli_fetch_assoc($sth)) {
-		//echo "id: " . $r["id"]. " - Name: " . $r["nombre"]. " " . $r["apellido"]. "<br>";
-		$rows[] = array_merge($r);
-	}
-	print json_encode($rows);
-	exit();
-}
-
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 	$info = (parse_url($url, PHP_URL_QUERY));
 	parse_str($info, $get_array);
 	//print_r($get_array);
 	//echo "Nombre: ",$get_array["nombre"],"Apellido: ",$get_array["apellido"];
-	$sql = "INSERT INTO usuario SET 
-	idTipoUsuario='$get_array[idTipoUsuario]', idEstatus='$get_array[idEstatus]', numeroTrabajador='$get_array[numeroTrabajador]', nombre='$get_array[nombre]', 
-	apellidoPaterno='$get_array[apellidoPaterno]', apellidoMaterno='$get_array[apellidoMaterno]', usuario='$get_array[usuario]', numeroOficina='$get_array[numeroOficina]', 
-	telefonoOficina='$get_array[telefonoOficina]', telefonoCasa='$get_array[telefonoCasa]', extension='$get_array[extension]', movil1='$get_array[movil1]', movil2='$get_array[movil2]', 
-	correo='$get_array[correo]'
-	";
+	$sql = "INSERT INTO prueba SET nombre='$get_array[nombre]' apellido='$get_array[apellido]' ";
 	if (mysqli_query($conn,$sql) === TRUE) {
 		echo "New record created successfully";
 		$result->status = "success";
@@ -40,31 +24,6 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 		$result->status = "failed";
 		$result->message = "User Not Added";
 		http_response_code(500);
-		print json_encode($result);
-	}
-	exit();
-}
-
-if($_SERVER['REQUEST_METHOD'] == 'PUT'){
-	echo "PUT";
-	exit();
-}
-
-if($_SERVER['REQUEST_METHOD'] == 'DELETE'){
-	//echo "DELETE";
-	//parse_str(file_get_contents("php://input"),$data);
-	$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-	$ok = parse_url($actual_link, PHP_URL_PATH);
-	$urlParameter = str_replace("/api.php/", "", $ok);
-	$sql = "DELETE FROM usuario WHERE idUsuario = '$urlParameter' ";
-	if($conn->query($sql) === TRUE){
-		$result->status = "success";
-		$result->message = "User Deleted";
-		print json_encode($result);
-	}else{
-		echo "Error: " . $sql . "<br>" . $conn->error;
-		$result->status = "failed";
-		$result->message = "User Not Deleted";
 		print json_encode($result);
 	}
 	exit();
