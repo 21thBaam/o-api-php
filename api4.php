1b506d8ebc7b40d071ff35f53374020781b33cde
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-25 02:45:57 -0500

    Update api4.php

--- api4.php
+++ api4.php
@@ -9,11 +9,21 @@ header('content-type: application/json; charset=utf-8');
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$post = file_get_contents('php://input');
 	$get_array = json_decode($post, true);
+	$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+	$txt = "\n";
+	fwrite($myfile, $post);
+	fwrite($myfile, $txt);
+	fwrite($myfile, $get_array["nombre"]);
+	fwrite($myfile, $txt);
+	fwrite($myfile, $get_array["apellido"]);
+	fwrite($myfile, $txt);
+	
 	$sql = "INSERT INTO prueba SET nombre='$get_array(nombre)', apellido='$get_array(apellido)' ";
 	if (mysqli_query($conn,$sql) === TRUE) {
 		echo "New record created successfully";
 		$result->status = "success";
 		$result->message = "User Added";
+		$result->ok = true;
 		http_response_code(201);
 		print json_encode($result);
 	} else {
