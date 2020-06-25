d6984a6c5c3147a5d6e8fbac77e31c6cfe6d63ab
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-24 23:19:08 -0500

    Update api4.php

--- api4.php
+++ api4.php
@@ -10,9 +10,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 	$info = (parse_url($url, PHP_URL_QUERY));
 	parse_str($info, $get_array);
-	print_r($get_array);
-	echo "Nombre: ",$get_array["nombre"],"Apellido: ",$get_array["apellido"];
-	$sql = " INSERT INTO prueba SET nombre='$get_array[nombre]' apellido='$get_array[apellido]' ";
+	//print_r($get_array);
+	//echo "Nombre: ",$get_array["nombre"],"Apellido: ",$get_array["apellido"];
+	$sql = " INSERT INTO prueba SET nombre='$get_array[nombre]', apellido='$get_array[apellido]' ";
 	if (mysqli_query($conn,$sql) === TRUE) {
 		echo "New record created successfully";
 		$result->status = "success";
