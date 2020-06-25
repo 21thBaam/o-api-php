84a2964ebdea54ac88663d7ce140ff7558f0e860
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-25 02:12:54 -0500

    Update api4.php

--- api4.php
+++ api4.php
@@ -7,14 +7,15 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('content-type: application/json; charset=utf-8');
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-	$url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-	$txt = "\n";
-	$myfile = fopen("newfile.txt", "w+") or die("Unable to open file!");
+	/*$url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+	
 	fwrite($myfile, $url);
 	fwrite($myfile, $txt);
 	$info = (parse_url($url, PHP_URL_QUERY));
 	fwrite($myfile, $info);
-	fwrite($myfile, $txt);
+	fwrite($myfile, $txt);*/
+	$txt = "\n";
+	$myfile = fopen("newfile.txt", "w+") or die("Unable to open file!");
 	$post = file_get_contents('php://input');
 	fwrite($myfile, $post);
 	fwrite($myfile, $txt);
