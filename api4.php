7372fbad6101fa44942799f8c5856c340c547f61
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-24 23:31:24 -0500

    Update api4.php

--- api4.php
+++ api4.php
@@ -8,11 +8,18 @@ header('content-type: application/json; charset=utf-8');
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-	//$info = (parse_url($url, PHP_URL_QUERY));
+	$txt = "\n";
+	$myfile = fopen("newfile.txt", "w+") or die("Unable to open file!");
+	fwrite($myfile, $url);
+	fwrite($myfile, $txt);
+	$info = (parse_url($url, PHP_URL_QUERY));
+	fwrite($myfile, $info);
+	fwrite($myfile, $txt);
 	//parse_str($info, $get_array);
 	//print_r($get_array);
 	//echo "Nombre: ",$get_array["nombre"],"Apellido: ",$get_array["apellido"];
 	http_response_code(201);
+	fclose($myfile);
 	exit();
 }
 ?>
