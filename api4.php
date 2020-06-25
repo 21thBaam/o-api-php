844fc411c8137c88e5c5e2d340f64cb84efd1258
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-25 03:02:46 -0500

    Update api4.php

--- api4.php
+++ api4.php
@@ -21,17 +21,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$sql = "INSERT INTO prueba SET nombre='$get_array[nombre]', apellido='$get_array[apellido]' ";
 	if (mysqli_query($conn,$sql) === TRUE) {
 		echo "New record created successfully";
-		$result->status = "success";
-		$result->message = "User Added";
-		$result->ok = true;
-		http_response_code(201);
-		print json_encode($result);
 	} else {
 		echo "Error: " . $sql . "<br>" . $conn->error;
-		$result->status = "failed";
-		$result->message = "User Not Added";
-		http_response_code(500);
-		print json_encode($result);
 	}
 	exit();
 }
