0e9f4e9fed7fb77cbdc4a100fcac280bf7419727
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-07-26 19:36:53 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "iwqrvsv8e5fz4uni.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-    $username = "el0gnvg0jiqbftm3";
-    $password = "xb76s2sub1phyj7q";
-    $dbname = "dspnarlgltkkzpxo";
+    $servername = "";
+    $username = "";
+    $password = "";
+    $dbname = "";
     
     $conn = new mysqli($servername, $username, $password, $dbname);
     
